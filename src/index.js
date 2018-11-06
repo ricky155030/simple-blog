fe9e9acfdc8b4dd47cba1915777be89384d888c8
HungWei Kao <hungwei.kao@gmail.com>
2018-11-06 16:32:21 +0000

    * Add Redux support

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'
+import configureStore from './configureStore'
 
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 
+const store = configureStore()
+
 ReactDOM.render(
   <Router>
-    <App /> 
+    <Provider store={store}>
+      <App /> 
+    </Provider>
   </Router>,
   document.getElementById('root')
 );
